9ae3d948390f1e2ab9f04232f6364ae2411868e0
Jasmin Biegemeier <jasmin.biegemeier@enpal.de>
2020-11-16 19:50:16 +0100

    first commit in react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import { useState } from "react";
+import { useState } from "react"
 import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNew] = useState("");
+  // create state handlers for the todo
+  const [newTodo, setNewTodo] = useState("")
 
+  // connect the state handler with the input
+  const handleChange = (e) => {
+    setNewTodo(e.target.value)
+  }
+
+  const [todos, setTodos] = useState(["cook", "clean"])
+
+  const handleClick = () => {
+
+    if (newTodo !== "") {
+      // create a new array that stores old todos and the new one
+      const newTodos = [...todos, newTodo]
+      // set the todos state to the newly created array
+      setTodos(newTodos)
+
+      // set the newTodo to "" (reset it)
+      setNewTodo("")
+    }
+  }
   return (
     <div>
       <div className="add-todo-box">
-        <input placeholder="Add a todo here..." className="input" type="text" />
-        <button className="button">Add todo</button>
+        <input value={newTodo} onChange={handleChange} placeholder="Add a todo here..." className="input" type="text" />
+        <button onClick={handleClick} className="button">Add todo</button>
       </div>
       <h3>Todos:</h3>
-      ... display todos here
+      {todos.map(todo => {
+        return <p className="todo">{todo}</p>
+      })
+      }
     </div>
   );
 }
